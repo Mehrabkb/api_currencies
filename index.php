f933349ee683e96c081dff8a0a3b21f6f2b300b2
mehrabkb <mkordbacheh75@gmail.com>
2024-02-11 11:54:52 +0330

    version 1

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+require __DIR__ . '/vendor/autoload.php';
 include('env.php');
 
 define("URL" , "https://api.nobitex.ir/v2/orderbook/all");
@@ -19,11 +20,61 @@ function beautify_data(){
     $data = get_data();
     $SYMBOLS = ['BTCIRT', 'ETHIRT', 'LTCIRT', 'USDTIRT', 'XRPIRT','BNBIRT', 'EOSIRT', 'XLMIRT','ETCIRT', 'TRXIRT', 'DOGEIRT', 'UNIIRT', 'DAIIRT', 'LINKIRT', 'DOTIRT',
      'AAVEIRT', 'ADAIRT', 'SHIBIRT', 'FTMIRT', 'MATICIRT', 'AXSIRT', 'MANAIRT', 'SANDIRT', 'AVAXIRT', 'MKRIRT', 
-     'GMTIRT', 'USDCIRT', 'BTCUSDT', 'ETHUSDT', 'LTCUSDT', 'XRPUSDT', 'BCHUSDT', 'BNBUSDT', 'EOSUSDT', 'XLMUSDT',
-      'ETCUSDT', 'TRXUSDT', 'PMNUSDT', 'DOGEUSDT', 'UNIUSDT', 'DAIUSDT', 'LINKUSDT', 'DOTUSDT', 'AAVEUSDT', 'ADAUSDT',
-       'SHIBUSDT', 'FTMUSDT', 'MATICUSDT', 'AXSUSDT', 'MANAUSDT', 'SANDUSDT', 'AVAXUSDT', 'MKRUSDT', 'GMTUSDT', 'USDCUSDT'];
+     'GMTIRT', 'USDCIRT'];
+    $finalData = [];
     foreach ($SYMBOLS as $key => $value) {
-        echo $value . '<br>';
+        // echo date('H:m:s m/d/Y ' , $data->$value->lastUpdate);
+        // $date = jdate($data->$value->lastUpdate)->format('datetime');
+        if($data->$value->lastTradePrice > 0){
+            array_push($finalData , [
+                'name' => $value,
+                'price' => $data->$value->lastTradePrice
+            ]);
+        }else{
+            array_push($finalData , [
+                'name' => $value ,
+                'price' => $data->$value->lastTradePrice
+            ]);
+        }
     }
+    return $finalData;
 }
-beautify_data();
+?>
+<!doctype html>
+<html lang="fa" dir="rtl">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>نرخ ارز دیجیتال</title>
+        <meta name="description" , content="نرخ ارزهای دیجیتال را میتوانید به راحتی از این صفحه بخوانید تمامی آپدیت از سایت نوبیتکس میباشد ">
+        <meta name="keywords" content="نرخ ارز , بازار , بیتکوین , اتریوم , دلار">
+        <meta name="author" content="mehrab kordbacheh , مهراب کردبچه">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+        <link rel="stylesheet" href="fonts/css/style.css">
+    </head>
+    <body>
+        <div class="container">
+        <h1 class="pt-3 pb-3">نرخ بازر ارز دیجیتال</h1>
+        <p>تمامی قیمت های زیر از سایت نوبیتکس استعلام گرفته شده میتوانید برای قیمت های بیشتر به این سایت مراجعه کنید</p>
+        <a href="https://nobitex.ir/" target="_blank">سایت نوبیتکس</a>
+        <div class="row">
+
+
+    <?php 
+       $fData = beautify_data();
+       foreach ($fData as $key => $value) {
+            echo '<div class="col-12 col-md-4 mt-3"><div class="card" style="width: 18rem;">
+            <div class="card-body">
+              <h5 class="card-title">'.$value['name'].'</h5>
+              <p class="card-text">'.number_format($value['price']).'  Rial </p>
+            </div>
+          </div></div>';
+       }
+    ?>
+                </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+  </body>
+</html>
